d80e23c92f37f26f18ce032cd6e5cab299247e6b
Muhammad Dimas <dimas.febrianto@globaltrendasia.com>
2024-12-12 16:24:17 +0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 30; i++)
+        {
+            String output = "";
+
+            if (i % 14 == 0 && i % 4 == 0)
+            {
+                output = "Unictive Media";
+            }
+            else if (i % 4 == 0)
+            {
+                output = "Unictive";
+            }
+            else 
+            {
+                output = i.ToString();
+            }
+            Console.WriteLine(output);
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
